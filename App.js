4ba83491300968a14d383ace536fd6c770311cc0
Bhanu <bhanu@gmail.com>
2024-09-14 19:10:13 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,3 @@
+let h1ref = React.createElement('h1', {}, 'Hello World Using React!');
+let divref = ReactDOM.createRoot(document.getElementById('root'));
+root.render(h1ref);
---
+++ hello.js
@@ -0,0 +1,20 @@
+function Hello() {
+  let refh1 = document.getElementById('heading');
+  let crelm = document.createElement('h1');
+  crelm.innerHTML = 'Hello World Program Using Javascript';
+  refh1.appendChild(crelm);
+}
+
+// function Hello() {
+//     // Get the reference to the element with ID 'heading'
+//     let refh1 = document.getElementById('heading');
+
+//     // Create a new <h1> element
+//     let crelm = document.createElement('h1');
+
+//     // Set the inner HTML of the new <h1> element
+//     crelm.innerHTML = 'Hello World Program Using Javascript';
+
+//     // Append the new <h1> element to the existing element
+//     refh1.appendChild(crelm);
+//   }
